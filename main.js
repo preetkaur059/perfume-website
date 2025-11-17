162254f8aa3904441bc7ac64e9b270eb3bf4c25c
preetkaur <parampreetkaur059@gmail.com>
2025-11-17 11:34:59 +0530

    changes

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    // lerp:1
+});
+
+
+
+// Initialize Lenis
+const lenis = new Lenis();
+
+// Use requestAnimationFrame to continuously update the scroll
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
 const hmbrger = document.getElementById("hamburger");
 const nvlist = document.getElementById("nav-list");
 
